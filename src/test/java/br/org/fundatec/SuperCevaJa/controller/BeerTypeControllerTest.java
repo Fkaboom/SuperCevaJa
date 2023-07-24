875841e9472530ef53464752809c53b9becb6aaa
João Gabriel C. da Cruz <gabriel_cassuriaga@hotmail.com>
2023-07-23 22:59:19 -0300

    testes corrigidos

--- src/test/java/br/org/fundatec/SuperCevaJa/controller/BeerTypeControllerTest.java
+++ src/test/java/br/org/fundatec/SuperCevaJa/controller/BeerTypeControllerTest.java
@@ -1,6 +1,7 @@
 package br.org.fundatec.SuperCevaJa.controller;
 
 import br.org.fundatec.SuperCevaJa.dto.beer.type.BeerTypeDTO;
+import br.org.fundatec.SuperCevaJa.dto.user.UserRequestCreateDTO;
 import br.org.fundatec.SuperCevaJa.service.BeerTypeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,12 +11,15 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 public class BeerTypeControllerTest {
@@ -28,56 +32,120 @@ public class BeerTypeControllerTest {
 
     @BeforeEach
     public void setup() {
-        // Configurações antes de cada teste, se necessário
     }
 
     @Test
     public void testCreateBeerSuccess() {
-        // Crie um UserRequestCreateDTO para simular o corpo da requisição
+        BeerTypeDTO beerTypeDTO = new BeerTypeDTO();
+        beerTypeDTO.setName("Brahma");
+        beerTypeDTO.setPrice(BigDecimal.valueOf(2));
+
+        doNothing().when(beerTypeService).createBeerType(beerTypeDTO);
+
+        ResponseEntity<String> responseEntity = beerTypeController.createBeerType(beerTypeDTO);
+
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        assertEquals("Beer " + beerTypeDTO.getName() + " was created.", responseEntity.getBody());
+
+        verify(beerTypeService, times(1)).createBeerType(beerTypeDTO);
+    }
+
+
+    @Test
+    public void testCreateRepeatedBeerFails() {
 
         BeerTypeDTO beerTypeDTO = new BeerTypeDTO();
         beerTypeDTO.setName("Brahma");
         beerTypeDTO.setPrice(BigDecimal.valueOf(2));
 
-        // Simule o comportamento do BeerService para o método createBeer
-        doNothing().when(beerTypeService).createBeerType(beerTypeDTO);
+        doThrow(new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                "Cannot add repeated Beer Type "  + beerTypeDTO.getName())).when(beerTypeService).createBeerType(beerTypeDTO);
 
-        // Chame o método da controller que você quer testar
         ResponseEntity<String> responseEntity = beerTypeController.createBeerType(beerTypeDTO);
 
-        // Verifique se a resposta HTTP é a esperada
-        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
-        assertEquals("Beer " + beerTypeDTO.getName()
-                + " was created.", responseEntity.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
 
-        // Verifique se o método do BeerTypeService foi chamado com os parâmetros corretos
+        String expectedErrorMessage = "Cannot add repeated Beer Type " + beerTypeDTO.getName();
+        String actualErrorMessage = responseEntity.getBody().replace("\"", "");
+        assertEquals(HttpStatus.BAD_REQUEST + " " + expectedErrorMessage, actualErrorMessage);
+
         verify(beerTypeService, times(1)).createBeerType(beerTypeDTO);
     }
 
     @Test
-    public void TestCreateRepeatedBeerFails() {
-        // Crie um UserRequestCreateDTO para simular o corpo da requisição
+    public void testDeleteBeerType_Success() {
+        // Defina um nome de tipo de cerveja válido para exclusão
+        String beerTypeNameToDelete = "IPA";
 
+        // Simule o comportamento do BeerTypeService para o método deleteBeerType
+        // Considerando que a exclusão é bem-sucedida
+        doNothing().when(beerTypeService).deleteBeerType(beerTypeNameToDelete);
+
+        // Chame o método da controller que você quer testar
+        ResponseEntity<String> responseEntity = beerTypeController.deleteBeerType(beerTypeNameToDelete);
+
+        // Verifique se a resposta HTTP é a esperada (202 - Accepted)
+        assertEquals(HttpStatus.ACCEPTED, responseEntity.getStatusCode());
+        assertEquals("Beer " + beerTypeNameToDelete + " has been deleted.", responseEntity.getBody());
+
+        // Verifique se o método do BeerTypeService foi chamado com o nome correto
+        verify(beerTypeService, times(1)).deleteBeerType(beerTypeNameToDelete);
+    }
+
+    @Test
+    public void testDeleteBeerType_Failure() {
+        // Defina um nome de tipo de cerveja inválido para exclusão
+        String nonExistingBeerTypeName = "NonExistingType";
+
+        // Simule o comportamento do BeerTypeService para o método deleteBeerType
+        // Considerando que a exclusão lança uma exceção
+        doThrow(new IllegalArgumentException("Beer Type not found with name " + nonExistingBeerTypeName))
+                .when(beerTypeService).deleteBeerType(nonExistingBeerTypeName);
+
+        // Chame o método da controller que você quer testar
+        ResponseEntity<String> responseEntity = beerTypeController.deleteBeerType(nonExistingBeerTypeName);
+
+        // Verifique se a resposta HTTP é a esperada (400 - Bad Request)
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertEquals("Beer Type not found with name " + nonExistingBeerTypeName, responseEntity.getBody());
+
+        // Verifique se o método do BeerTypeService foi chamado com o nome correto
+        verify(beerTypeService, times(1)).deleteBeerType(nonExistingBeerTypeName);
+    }
+
+    @Test
+    public void testFindAll() {
+        // Crie uma lista de BeerTypeDTO para simular o retorno do serviço
+        List<BeerTypeDTO> beerTypes = new ArrayList<>();
         BeerTypeDTO beerTypeDTO = new BeerTypeDTO();
         beerTypeDTO.setName("Brahma");
         beerTypeDTO.setPrice(BigDecimal.valueOf(2));
-        // Simule o comportamento do beerTypeService para o método createBeer
-        doNothing().when(beerTypeService).createBeerType(beerTypeDTO);
 
-        BeerTypeDTO repetedBeer = new BeerTypeDTO();
-        repetedBeer.setName("Brahma");
-        repetedBeer.setPrice(BigDecimal.valueOf(2));
+        BeerTypeDTO beerTypeDTO2 = new BeerTypeDTO();
+        beerTypeDTO2.setName("Itaipava");
+        beerTypeDTO2.setPrice(BigDecimal.valueOf(2));
+        beerTypes.add(beerTypeDTO);
+        beerTypes.add(beerTypeDTO2);
+        // Simule o comportamento do BeerTypeService para o método findAll
+        when(beerTypeService.findAll()).thenReturn(beerTypes);
 
-        doNothing().when(beerTypeService).createBeerType(repetedBeer);
-        ResponseEntity<String> responseEntity = beerTypeController.createBeerType(beerTypeDTO);
+        // Chame o método da controller que você quer testar
+        ResponseEntity<List<BeerTypeDTO>> responseEntity = beerTypeController.findAll();
 
-        // Verifique se a resposta HTTP é a esperada
-        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-        assertEquals("Beer " + beerTypeDTO.getName()
-                + " was created.", responseEntity.getBody());
+        // Verifique se a resposta HTTP é a esperada (200 - OK)
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
 
-        // Verifique se o método do UserService foi chamado com os parâmetros corretos
-        verify(beerTypeService, times(1)).createBeerType(beerTypeDTO);
+        // Verifique se o método do BeerTypeService foi chamado
+        verify(beerTypeService, times(1)).findAll();
+
+        // Verifique se a lista de BeerTypeDTO retornada na resposta é a mesma do serviço
+        List<BeerTypeDTO> returnedBeerTypes = responseEntity.getBody();
+        assertEquals(beerTypes.size(), returnedBeerTypes.size());
+        assertEquals(beerTypes.get(0).getName(), returnedBeerTypes.get(0).getName());
+        assertEquals(beerTypes.get(0).getPrice(), returnedBeerTypes.get(0).getPrice());
+        assertEquals(beerTypes.get(1).getName(), returnedBeerTypes.get(1).getName());
+        assertEquals(beerTypes.get(1).getPrice(), returnedBeerTypes.get(1).getPrice());
     }
-
 }
+
+
--- src/test/java/br/org/fundatec/SuperCevaJa/controller/UserControllerTest.java
+++ src/test/java/br/org/fundatec/SuperCevaJa/controller/UserControllerTest.java
@@ -3,6 +3,7 @@ package br.org.fundatec.SuperCevaJa.controller;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import br.org.fundatec.SuperCevaJa.dto.user.UserDTO;
 import br.org.fundatec.SuperCevaJa.dto.user.UserRequestCreateDTO;
 import br.org.fundatec.SuperCevaJa.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,8 +14,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 public class UserControllerTest {
@@ -27,38 +31,32 @@ public class UserControllerTest {
 
     @BeforeEach
     public void setup() {
-        // Configurações antes de cada teste, se necessário
     }
 
     @Test
     public void testCreateUserSuccess() {
-        // Crie um UserRequestCreateDTO para simular o corpo da requisição
+
         UserRequestCreateDTO userRequestCreateDTO = new UserRequestCreateDTO();
-        userRequestCreateDTO.setName("Gilton");
-        userRequestCreateDTO.setSurname("Alanis");
+
+        userRequestCreateDTO.setName("Jilton");
+        userRequestCreateDTO.setSurname("Alamis");
         userRequestCreateDTO.setPassword("mysonphilip123");
         userRequestCreateDTO.setLogin("militarmaromba");
         userRequestCreateDTO.setBirthDate(LocalDate.parse("1970-01-01"));
         userRequestCreateDTO.setCpf("011.208.800-76");
 
-        // Simule o comportamento do UserService para o método createUser
         doNothing().when(userService).createUser(userRequestCreateDTO);
 
-        // Chame o método da controller que você quer testar
         ResponseEntity<String> responseEntity = userController.createUser(userRequestCreateDTO);
 
-        // Verifique se a resposta HTTP é a esperada
         assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
         assertEquals("User " + userRequestCreateDTO.getLogin() + " was created.", responseEntity.getBody());
 
-        // Verifique se o método do UserService foi chamado com os parâmetros corretos
         verify(userService, times(1)).createUser(userRequestCreateDTO);
     }
 
     @Test
-    public void TestCreateRepeatedUserFails() {
-        // Crie um UserRequestCreateDTO para simular o corpo da requisição
-
+    public void testCreateUserDuplicateLogin() {
         UserRequestCreateDTO userRequestCreateDTO = new UserRequestCreateDTO();
         userRequestCreateDTO.setName("Jilton");
         userRequestCreateDTO.setSurname("Alamis");
@@ -66,53 +64,117 @@ public class UserControllerTest {
         userRequestCreateDTO.setLogin("militarmaromba");
         userRequestCreateDTO.setBirthDate(LocalDate.parse("1970-01-01"));
         userRequestCreateDTO.setCpf("011.208.800-76");
-        // Simule o comportamento do UserService para o método createUser
-        doNothing().when(userService).createUser(userRequestCreateDTO);
 
-        UserRequestCreateDTO repeatedUser = new UserRequestCreateDTO();
-        repeatedUser.setName("Dio");
-        repeatedUser.setSurname("Brando");
-        repeatedUser.setPassword("mydadjilton123");
-        userRequestCreateDTO.setLogin("militarmaromba");
-        userRequestCreateDTO.setBirthDate(LocalDate.parse("2004-01-01"));
-        userRequestCreateDTO.setCpf("011.209.800-76");
-
-        doNothing().when(userService).createUser(repeatedUser);
-        ResponseEntity<String> responseEntity = userController.createUser(repeatedUser);
-
-        // Verifique se a resposta HTTP é a esperada
-        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-        assertEquals("User already exists with login " + repeatedUser.getLogin(), responseEntity.getBody());
-
-        // Verifique se o método do UserService foi chamado com os parâmetros corretos
-        verify(userService, times(2)).createUser(userRequestCreateDTO);
-    }
-
-    @Test
-    public void testGetUserSuccess() {
-        // Crie um UserRequestCreateDTO para simular o corpo da requisição
-        UserRequestCreateDTO userRequestCreateDTO = new UserRequestCreateDTO();
-        userRequestCreateDTO.setName("Gilton");
-        userRequestCreateDTO.setSurname("Alanis");
-        userRequestCreateDTO.setPassword("mysonphilip123");
-        userRequestCreateDTO.setLogin("militarmaromba");
-        userRequestCreateDTO.setBirthDate(LocalDate.parse("1970-01-01"));
-        userRequestCreateDTO.setCpf("011.208.800-76");
-
-        // Simule o comportamento do UserService para o método createUser
-        doNothing().when(userService).createUser(userRequestCreateDTO);
-
-        doNothing().when(userController).findById(1L);
+        doThrow(new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                "User already exists with login " + userRequestCreateDTO.getLogin())).when(userService).createUser(userRequestCreateDTO);
 
         ResponseEntity<String> responseEntity = userController.createUser(userRequestCreateDTO);
 
-        // Verifique se a resposta HTTP é a esperada
-        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
-        assertEquals("User " + userRequestCreateDTO.getLogin() + " was created.", responseEntity.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
 
-        // Verifique se o método do UserService foi chamado com os parâmetros corretos
+        String expectedErrorMessage = "User already exists with login " + userRequestCreateDTO.getLogin();
+        String actualErrorMessage = responseEntity.getBody().replace("\"", ""); // Remover as aspas do resultado
+        assertEquals(HttpStatus.BAD_REQUEST + " " + expectedErrorMessage, actualErrorMessage);
+
         verify(userService, times(1)).createUser(userRequestCreateDTO);
     }
 
+    @Test
+    public void testFindByIdSuccess() {
+        Long userId = 1L;
 
+        UserDTO userDto = new UserDTO();
+        when(userService.findByIdDto(userId)).thenReturn(userDto);
+
+        ResponseEntity<UserDTO> responseEntity = userController.findById(userId);
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+
+        UserDTO expectedUserDto = userDto;
+        UserDTO actualUserDto = responseEntity.getBody();
+        assertEquals(expectedUserDto, actualUserDto);
+
+        verify(userService, times(1)).findByIdDto(userId);
+    }
+
+    @Test
+    public void testFindAllSuccess() {
+
+        List<UserDTO> userDtoList = new ArrayList<>();
+
+
+        UserDTO userDTO1 = new UserDTO();
+        userDTO1.setName("John");
+        userDTO1.setSurname("Doe");
+        userDTO1.setCpf("123.456.789-00");
+        userDTO1.setLogin("john.doe");
+
+        UserDTO userDTO2 = new UserDTO();
+        userDTO2.setName("Jane");
+        userDTO2.setSurname("Smith");
+        userDTO2.setCpf("987.654.321-00");
+        userDTO2.setLogin("jane.smith");
+
+        userDtoList.add(userDTO1);
+        userDtoList.add(userDTO2);
+
+        when(userService.findAll()).thenReturn(userDtoList);
+
+        ResponseEntity<List<UserDTO>> responseEntity = userController.findAll();
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+
+        List<UserDTO> expectedUserDtoList = userDtoList;
+        List<UserDTO> actualUserDtoList = responseEntity.getBody();
+        assertEquals(expectedUserDtoList, actualUserDtoList);
+
+        verify(userService, times(1)).findAll();
+    }
+
+    @Test
+    public void testDeleteUser_Success() {
+        // Defina um login de usuário válido para exclusão
+        String userLoginToDelete = "john_doe";
+
+        // Simule o comportamento do UserService para o método deleteUser
+        // Considerando que a exclusão é bem-sucedida
+        doNothing().when(userService).deleteUser(userLoginToDelete);
+
+        // Chame o método da controller que você quer testar
+        ResponseEntity<String> responseEntity = userController.deleteUser(userLoginToDelete);
+
+        // Verifique se a resposta HTTP é a esperada (202 - Accepted)
+        assertEquals(HttpStatus.ACCEPTED, responseEntity.getStatusCode());
+        assertEquals("User " + userLoginToDelete + " has been deleted.", responseEntity.getBody());
+
+        // Verifique se o método do UserService foi chamado com o login correto
+        verify(userService, times(1)).deleteUser(userLoginToDelete);
+    }
+
+
+    @Test
+    public void testDeleteUser_Failure() {
+        // Defina um login de usuário inválido para exclusão
+        String nonExistingUserLogin = "non_existing_user";
+
+        // Simule o comportamento do UserService para o método deleteUser
+        // Considerando que a exclusão lança uma exceção
+        doThrow(new IllegalArgumentException("User not found with login " + nonExistingUserLogin))
+                .when(userService).deleteUser(nonExistingUserLogin);
+
+        // Chame o método da controller que você quer testar
+        ResponseEntity<String> responseEntity = userController.deleteUser(nonExistingUserLogin);
+
+        // Verifique se a resposta HTTP é a esperada (400 - Bad Request)
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertEquals("User not found with login " + nonExistingUserLogin, responseEntity.getBody());
+
+        // Verifique se o método do UserService foi chamado com o login correto
+        verify(userService, times(1)).deleteUser(nonExistingUserLogin);
+    }
 }
+
+
+
+
+
